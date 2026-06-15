cf5b501e67d0b0a389a9f3d555fccbe3d7ff49be
Chay Qian <richychay02@gmail.com>
2026-06-15 21:09:08 +0800

    Push all changes

--- worker.js
+++ worker.js
@@ -1,5 +1,10 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === "/" && !url.searchParams.has("v")) {
+      url.searchParams.set("v", "20260611a");
+      return Response.redirect(url.toString(), 302);
+    }
     return env.ASSETS.fetch(request);
   },
 };
